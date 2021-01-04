dbc7bae06d0cc6ccfba23350c081229fa0cbb3be
bcdguz <bcdguz96@gmail.com>
2021-01-03 16:33:37 -0800

    Creating users table and model

--- W7D1/auth/app/models/user.rb
+++ W7D1/auth/app/models/user.rb
@@ -1,2 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    validates :username, presence: true
+    validates :password_digest, presence: {message: "Password can't be blank"}
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+
+
+
 end
---
+++ W7D1/auth/db/migrate/20210104003019_remove_index_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveIndexFromUsers < ActiveRecord::Migration[5.2]
+  def change
+  
+  end
+end
