09ca7150726595f0ab08e9dd7a2ba8445fd3e2b9
bcdguz <bcdguz96@gmail.com>
2021-01-14 20:01:46 -0800

    Adding teaAndBiscuits()

---
+++ W8D5/hw/cb_and_functions_pt2.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function teaAndBiscuits() {
+    reader.question('Would you like some tea?', function(answer) {
+        console.log(`You replied ${answer}`);
+    });
+    reader.close();
+}
+
+
+function teaAndBiscuits2() {
+    let first, second;
+
+    reader.question('Would you like some tea?', (res) => {
+        first = res;
+        console.log(`You replied ${res}.`);
+        reader.question('Would you like some biscuits?', (res) => {
+            second = res;
+            console.log(`You replied ${res}.`);
+            const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+            const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+            console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+            reader.close();
+        });
+    });
+    
+}
+
+// teaAndBiscuits2();
