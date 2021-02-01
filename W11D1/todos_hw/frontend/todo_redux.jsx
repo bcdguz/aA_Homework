be344ef395243dde60840ac846c1b860be0db868
bcdguz <bcdguz96@gmail.com>
2021-01-31 17:24:16 -0800

    Creating logging function for dispatching actions

--- W11D1/todos_hw/frontend/todo_redux.jsx
+++ W11D1/todos_hw/frontend/todo_redux.jsx
@@ -8,7 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
-
+  store.dispatch = addLoggingToDispatch(store);
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+const addLoggingToDispatch = store => action => {
+  console.log(store.getState()); //old state
+  console.log(action); //POJO to be dispatched
+  store.dispatch(action);
+  console.log(store.getState()); //new state
+}
