a6430dfddd7a211fdb3df6dad30d2f5fcc4a34f9
bcdguz <bcdguz96@gmail.com>
2021-01-31 17:45:46 -0800

    Adding applyMiddleware function in configureStore

--- W11D1/todos_hw/frontend/store/store.js
+++ W11D1/todos_hw/frontend/store/store.js
@@ -1,12 +1,27 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  let store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
+  
   return store;
 }
+
+
+const addLoggingToDispatch = store => next => action => {
+
+  console.log('Action received:', action);
+  console.log('State pre-dispatch:', store.getState());
+
+  let result = next(action);
+
+  console.log('State post-dispatch:', store.getState());
+
+  return result;
+};
 
 export default configureStore;
