85311bc436ba4b8af9da0b253e58cf9a584438c0
bcdguz <bcdguz96@gmail.com>
2021-02-01 19:29:57 -0800

    Adding giphy_search_container

--- W11D2/giphy/giphy_search.jsx
+++ W11D2/giphy/giphy_search.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 // import { fetchSearchGiphys } from './util/api_util';
-import { fetchSearchGiphys } from './actions/giphy_actions';
+// import { fetchSearchGiphys } from './actions/giphy_actions';
 
 
 document.addEventListener("DOMContentLoaded", () => {
     
     const store = configureStore();
-    window.store = store;
-    window.fetchSearchGiphys = fetchSearchGiphys;
+    const root = document.getElementById("root");
+    ReactDOM.render(<Root store={store}/>, root);
     
 })
